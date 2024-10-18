a625b1c36a67486c2054412885d7ac43cd551328
z13327875970 <z13327875970@gmail.com>
2024-10-18 19:16:06 +0800

    no message

---
+++ Assets/Scripts/RayInterface.cs
@@ -0,0 +1,7 @@
+namespace RayInterface 
+{
+    interface IRay
+    {
+        void Ray();        
+    }
+}
---
+++ Assets/Scripts/ReflectionMode.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RayInterface;
+
+public class ReflectionMode : MonoBehaviour,IRay
+{
+    public int distance;//碰撞后的剩余长度
+    public void Ray()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
