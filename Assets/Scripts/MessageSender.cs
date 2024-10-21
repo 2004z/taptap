b65c32c8c0548b8a4c11fb2df180d0da5a62a95e
z13327875970 <z13327875970@gmail.com>
2024-10-21 21:49:19 +0800

    no message

--- Assets/Scripts/MessageSender.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class MessageSender 
-{
-    public abstract void SendMessage(float distance, GameObject recipient);
-}
